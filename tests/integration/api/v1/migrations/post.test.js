0ae104909333cf0a81cc8a0934dfe2069cc31d6e
Erick Sousa Gomes <98856987+ericksoumes@users.noreply.github.com>
2024-12-18 19:38:02 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,38 +6,51 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        expect(Array.isArray(response1Body)).toBe(true);
+
+        expect(response1Body.length).toBeGreaterThan(0);
+
+        expect(response1Body[0]).toHaveProperty("path");
+
+        expect(response1Body[0]).toHaveProperty("name");
+
+        expect(response1Body[0]).toHaveProperty("timestamp");
+
+        expect(typeof response1Body[0].path).toBe("string");
+
+        expect(typeof response1Body[0].name).toBe("string");
+
+        expect(typeof response1Body[0].timestamp).toBe("number");
+      });
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  expect(response1Body[0]).toHaveProperty("path");
-
-  expect(response1Body[0]).toHaveProperty("name");
-
-  expect(response1Body[0]).toHaveProperty("timestamp");
-
-  expect(typeof response1Body[0].path).toBe("string");
-
-  expect(typeof response1Body[0].name).toBe("string");
-
-  expect(typeof response1Body[0].timestamp).toBe("number");
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-
-  expect(response2Body.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,28 +4,34 @@ beforeAll(async () => {
   await orchestrator.waitFotAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(typeof responseBody.dependencies.database.postgres_version).toBe(
-    "string",
-  );
-  expect(responseBody.dependencies.database.postgres_version).toBe("16.0");
+      expect(typeof responseBody.dependencies.database.postgres_version).toBe(
+        "string",
+      );
+      expect(responseBody.dependencies.database.postgres_version).toBe("16.0");
 
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+      expect(typeof responseBody.dependencies.database.max_connections).toBe(
+        "number",
+      );
+      expect(
+        responseBody.dependencies.database.max_connections,
+      ).toBeGreaterThan(0);
 
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      expect(typeof responseBody.dependencies.database.opened_connections).toBe(
+        "number",
+      );
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
