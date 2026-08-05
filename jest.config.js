b89d69d966a56ba37c84717d08bb59bf139d19a9
ericksoumes <erigomes05@gmail.com>
2026-08-04 21:24:07 -0300

    chore: upgrade node-pg-migrate

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,9 @@ dotenv.config({
   path: ".env.development",
 });
 
-const createJestConfig = nextJest();
+const createJestConfig = nextJest({
+  dir: "./",
+});
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
   testTimeout: 60000,
---
+++ next.config.js
@@ -0,0 +1,5 @@
+const nextConfig = {
+  transpilePackages: ["node-pg-migrate"],
+};
+
+module.exports = nextConfig;
