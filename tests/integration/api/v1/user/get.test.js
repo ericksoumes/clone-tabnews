d4db59c27bac16770507c5b709038dbc502a0235
ericksoumes <erigomes05@gmail.com>
2026-09-22 11:09:53 -0300

    feat: disable cache for `/user` endpoint

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -26,6 +26,11 @@ describe("GET /api/v1/user", () => {
 
       expect(response.status).toBe(200);
 
+      const CacheControl = response.headers.get("Cache-Control");
+      expect(CacheControl).toBe(
+        "no-store, no-cache, max-age=0, must-revalidate",
+      );
+
       const responseBody = await response.json();
 
       expect(responseBody).toEqual({
